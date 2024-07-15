cfbbd7c812d21b9ff03002d408ea668eea27059a
chanmi1213 <166026933+chanmi1213@users.noreply.github.com>
2024-07-15 21:17:47 +0900

    create gallery page

---
+++ src/main/java/com/example/sportsMatching/SportsMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportsMatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsMatchingApplication.class, args);
+	}
+
+}
